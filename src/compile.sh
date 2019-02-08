dad2f7f01b3be5f2c55ebd4af5d097ae635463c3
sautedman <bowersd@ucla.edu>
2019-02-08 16:28:21 -0700

    added possibility of making hfst ... but unexpected semicolon errors were getting thrown, so left dormant

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,6 @@
 cat morphology/root.lexc morphology/stems/adverbs.lexc morphology/stems/closedclassprotection.lexc morphology/stems/nouns.lexc morphology/stems/numerals.lexc morphology/stems/particles.lexc morphology/stems/prefixes.lexc morphology/stems/pronouns.lexc morphology/stems/punctuation.lexc morphology/stems/quantifiers.lexc morphology/stems/vaio.lexc morphology/stems/verbs.lexc morphology/affixes/nouns.lexc morphology/affixes/personprefixes.lexc morphology/affixes/verbs.lexc > tmp.lexc
 
-xfst -f basic_compile.xfscript
+xfst -f basic-compile.xfscript
+#hfst-xfst -F basic-compile.xfscript
 
 rm tmp.lexc
