5611a303d68ef32bbcb83407a8ccf0555be1dd1b
sautedman <bowersd@email.arizona.edu>
2023-10-06 07:34:37 -0700

    updated file name

--- debugging/get_split_entries.py
+++ debugging/get_split_entries.py
@@ -1,10 +1,13 @@
 import re
 
-with open("../src/morphology/lexicon.lexc.tmp") as file_in:
+with open("../src/morphology/lexicon.tmp.lexc") as file_in:
     keyed = {}
     for line in file_in:
         key = re.search("key: n[^ ]*", line)
         if key and key[0] not in keyed: keyed[key[0]] = [line]
         elif key and key[0] in keyed: keyed[key[0]].append(line)
-
-
+    for key in keyed:
+        if len(keyed[key]) > 1:
+            print()
+            for s in keyed[key]:
+                print(s)
