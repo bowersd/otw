f711a467490eaf9ae6cc253d646e4128752b0105
sautedman <bowersd@email.arizona.edu>
2023-10-06 14:14:51 -0700

    modified regex to hit, added basic counting information

--- debugging/get_split_entries.py
+++ debugging/get_split_entries.py
@@ -3,11 +3,14 @@ import re
 with open("../src/morphology/lexicon.tmp.lexc") as file_in:
     keyed = {}
     for line in file_in:
-        key = re.search("key: n[^ ]*", line)
+        key = re.search("key:n[^ ]*", line)
         if key and key[0] not in keyed: keyed[key[0]] = [line]
         elif key and key[0] in keyed: keyed[key[0]].append(line)
+    cnt = 0
     for key in keyed:
         if len(keyed[key]) > 1:
+            cnt += 1
             print()
             for s in keyed[key]:
-                print(s)
+                print(s.strip())
+    print(cnt)
