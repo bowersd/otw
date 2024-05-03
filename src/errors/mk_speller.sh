340407834f0ff2224f2260bec33b77d7a1898e63
sautedman <sautedman@protonmail.com>
2024-05-02 20:43:49 -0700

    a build pipeline. I tried inverted and uninverted error models,  but neither one results in suggestions on the cli

---
+++ src/errors/mk_speller.sh
@@ -0,0 +1,10 @@
+#not bothering to take the upper projection
+#the result of doing that is that fst2fst throws and error
+#hfst-project --project=upper 
+hfst-xfst -F errormodel.default.xfscript
+hfst-invert -i errormodel.hfst -o errormodel.default.hfst
+hfst-fst2fst -i errormodel.default.hfst -o errmodel.default.hfst -f olw
+hfst-fst2fst -i ../morphophonologyclitics_analyze.hfst -o acceptor.default.hfst -f olw
+
+rm spellchecker.zhfst
+zip spellchecker.zhfst index.xml acceptor.default.hfst errmodel.default.hfst
